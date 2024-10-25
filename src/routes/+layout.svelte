d294e1e0dd65c8f60bfe7e1f17e88f10d4f6350b
Noam Siegel <52804845+noamsiegel@users.noreply.github.com>
2024-10-25 11:36:07 -0700

    Merge pull request #2 from noamsiegel/Svelte-routes-&-pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import "../app.css";
+  import BackButton from "../lib/components/BackButton.svelte";
+</script>
+
+<!-- <slot></slot>-->
+
+<header>
+  <BackButton />
+</header>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  header {
+    padding: 16px;
+    background-color: #f8f8f8;
+  }
+
+  main {
+    padding: 16px;
+  }
+</style>
