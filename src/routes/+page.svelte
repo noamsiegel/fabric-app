3a56d16502022ec1e344179f5592bfca3e66605c
Noam Siegel <52804845+noamsiegel@users.noreply.github.com>
2024-10-25 18:42:03 -0700

    Merge pull request #13 from noamsiegel/Added-search-to-select-patterns
    Added search to select patterns

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,26 @@
   // svelte stores
   import { onMount } from "svelte";
   import { invoke } from "@tauri-apps/api/core";
-  import { writable, get } from "svelte/store";
+  import { writable, get, derived } from "svelte/store";
   import type { Writable } from "svelte/store";
 
   // svelte components
   import { Input } from "$lib/components/ui/input";
   import { Button } from "$lib/components/ui/button";
   import { Label } from "$lib/components/ui/label";
-  import * as Select from "$lib/components/ui/select";
   import { Settings, Home, FileText, PlayCircle } from "lucide-svelte";
   import { Textarea } from "$lib/components/ui/textarea";
-
-  // tauri plugins
-  import { Command } from "@tauri-apps/plugin-shell";
   import {
     runPattern,
     scrapeUrl,
     searchQuestion,
     scrapeAndRunPattern,
   } from "$lib/fabricCommands";
+  import PatternCombobox from "$lib/components/PatternCombobox.svelte";
+
+  function handlePatternSelect(event: CustomEvent<string>) {
+    selected.set({ value: event.detail, label: event.detail });
+  }
 
   // let selectedPattern = "";
   let selected: Writable<{ value: string; label: string }> = writable({
@@ -36,6 +37,7 @@
   onMount(async () => {
     try {
       patterns = await invoke("get_patterns");
+      console.log("Patterns:", patterns);
       const savedPattern = await invoke("get_selected_pattern");
       if (typeof savedPattern === "string" && savedPattern) {
         selected.set({ value: savedPattern, label: savedPattern });
@@ -68,6 +70,10 @@
         "An error occurred while scraping and running the pattern.";
     }
   }
+
+  $: {
+    console.log("Patterns updated:", patterns);
+  }
 </script>
 
 <div class="flex flex-col h-screen">
@@ -94,18 +100,13 @@
       </div>
       <div class="mb-6">
         <Label for="pattern-select">Select Pattern</Label>
-        <Select.Root bind:selected={$selected}>
-          <Select.Trigger id="pattern-select">
-            <Select.Value placeholder="Choose a pattern" />
-          </Select.Trigger>
-          <Select.Content>
-            {#each patterns as pattern}
-              <Select.Item value={pattern}>{pattern}</Select.Item>
-            {/each}
-          </Select.Content>
-        </Select.Root>
+        <!-- TODO make this  auto refresh the drop down items -->
+        <PatternCombobox
+          {patterns}
+          selectedPattern={$selected.value}
+          on:select={handlePatternSelect}
+        />
       </div>
-
       <div class="mb-6">
         <Label for="url-input">Enter URL to Scrape</Label>
         <div class="flex space-x-2">
