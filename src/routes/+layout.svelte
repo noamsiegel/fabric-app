0cda65b7830b1f3d683e46d4fc2eedf01d5294fb
Noam Siegel <52804845+noamsiegel@users.noreply.github.com>
2024-10-30 17:15:57 -0700

    Merge pull request #54 from noamsiegel/Todoist-test-2
    test 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,15 @@
   import Header from "./Header.svelte";
 </script>
 
+<!-- todoist test 4 -->
+
+<!-- TODO todoist test 3 -->
+<!-- Issue URL: https://github.com/noamsiegel/fabric-app/issues/60 -->
+<!-- Issue URL: https://github.com/noamsiegel/fabric-app/issues/58 -->
+<!-- Issue URL: https://github.com/noamsiegel/fabric-app/issues/56 -->
+
+
+
 <!-- TODO make it so that all front end pages have the same width margins -->
 <!-- Issue URL: https://github.com/noamsiegel/fabric-app/issues/53 -->
 <!-- Issue URL: https://github.com/noamsiegel/fabric-app/issues/52 -->
