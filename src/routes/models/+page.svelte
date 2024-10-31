ed7fa4852aa606574986564fb940e07fbe35b82a
Noam Siegel <52804845+noamsiegel@users.noreply.github.com>
2024-10-31 15:20:51 -0700

    Merge pull request #100 from noamsiegel/Table-GUI-constancy
    refactor: Improve layout structure in main pages.

--- src/routes/models/+page.svelte
+++ src/routes/models/+page.svelte
@@ -7,7 +7,7 @@
 
 <!-- TODO load faster by storing models in file and having a refresh button -->
 
-<div class="container py-8">
+<div>
   <h1 class="text-3xl font-bold mb-6">Models</h1>
 
   <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
